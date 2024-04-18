461e4fffae92cb80db96224b8ee9e2044fab2e00
jp <jsyslo@proton.me>
2024-04-17 21:56:27 -0500

    work ?!

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -3,17 +3,12 @@ import Link from 'next/link';
 import Logo from '../../public/logo.png'
 import NavLinks from './nav-links';
 
-const links = [
-    {
-
-    }
-]
 export default function Navbar() {
     return (
         <header className="h-20">
-            <div className="flex justify-center items-center text-2xl font-bold py-4 2xl:px-16">
+            <div className="flex justify-center items-center text-3xl  py-4 2xl:px-16">
                 <Link href="/">
-                    John Syslo
+                    <h1>John Syslo</h1>
                 </Link>
             </div>
             <nav className="flex justify-center">
