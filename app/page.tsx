89ba77fef69fb8a575c245421c036cfc4c91f5d5
jp <jsyslo@proton.me>
2024-04-18 06:40:36 -0500

    making it better, i hope..

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main>
       <Navbar />
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus, a consequatur soluta non impedit harum obcaecati iste eum ab laborum rerum eos quia reiciendis debitis. Nisi dolore animi magni earum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, recusandae inventore enim natus non itaque nesciunt. Exercitationem hic, error inventore obcaecati ut iusto sapiente perspiciatis totam, officia magnam eius neque! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab autem omnis beatae, ducimus fuga, provident delectus porro libero, cum nemo quos. Blanditiis praesentium dicta, officiis cum animi et commodi odio.</p>
     </main>
 
   );
