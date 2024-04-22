fb7fa920d3b67231f333fcc18185effb990d8a39
jp <jsyslo@proton.me>
2024-04-21 22:06:59 -0500

    feat: added /about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from '@/components/navbar/Nav';
+import About from '@/constants/text/About';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <About />
+    </>
+  );
+}
