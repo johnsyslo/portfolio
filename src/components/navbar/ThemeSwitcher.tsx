3b039285a98ec6675ad85462a1982f4dc50eee3d
jp <jsyslo@proton.me>
2024-04-26 19:49:42 -0500

    test: theme-toggle

---
+++ src/components/navbar/ThemeSwitcher.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { LuSun, LuMoon } from 'react-icons/lu';
+import { useState, useEffect } from 'react';
+import { useTheme } from 'next-themes';
+
+export default function ThemeSwitch() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return (
+    <span className="m-5"></span>
+  );
+
+  if (theme === 'dark') {
+    return (
+      <LuSun className="mx-3 text-xl hover:text-violet-600" onClick={() => setTheme('light')} />
+    );
+  }
+
+  if (theme === 'light') {
+    return (
+      <LuMoon className="mx-3 text-xl hover:text-violet-600" onClick={() => setTheme('dark')} />
+    );
+  }
+  return null;
+}
